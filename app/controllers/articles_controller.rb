a66bd29a3560c081a3450767b7cb2144e6581a39
Daljit Singh <daljitsinghmac@gmail.com>
2018-03-06 11:43:42 +0530

    Add edit , update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		#render plain: params[:article].inspect
-		#output: {"title"=>"test", "description"=>"test1"}
-		#@article = Article.new(article_params)
-		#@article.save
-		#redirect_to_articles_show(@article)
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was successfully created"
@@ -17,6 +12,21 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+	def edit
+		@article = Article.find(params[:id])		
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+
 
 	def show
 		@article = Article.find(params[:id])
