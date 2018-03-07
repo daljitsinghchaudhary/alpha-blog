ecb2f49d4833d65b438c183377fd502a33a2d197
Daljit Singh <daljitsinghmac@gmail.com>
2018-03-07 13:49:52 +0530

    style form partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
-class WelcomeController < ActionController::Base
+#class WelcomeController < ActionController::Base//Call static page with without header and footer
+class WelcomeController < ApplicationController
 	def home
 
 	end
