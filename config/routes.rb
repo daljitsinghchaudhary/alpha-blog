75986d4e530bc36cde024abecf09fdf63478b0b8
Daljit Singh <daljitsinghmac@gmail.com>
2018-03-09 12:46:06 +0530

    create user login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,12 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
