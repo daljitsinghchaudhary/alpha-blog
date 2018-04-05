322a927ef4f6fc30dd68bff4adc6cf81e6757be3
Daljit Singh <daljitsinghmac@gmail.com>
2018-04-05 17:19:49 +0530

    Update all view with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,7 @@
 class ArticlesController < ApplicationController
 
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
-
 	before_action :require_user, except: [:index, :show]
-
 	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
@@ -51,7 +49,8 @@ class ArticlesController < ApplicationController
 			@article = Article.find(params[:id])
 		end
 		def article_params
-			params.require(:article).permit(:title, :description)
+			#params.require(:article).permit(:title, :description)
+		  params.require(:article).permit(:title, :description, category_ids: [])
 		end
 
 	 	def require_same_user
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,6 @@
 class Category < ActiveRecord::Base 
-
-	# (if Rails 5 -> class Category < ApplicationRecord)
 	has_many :article_categories
 	has_many :articles, through: :article_categories
 	validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 	validates_uniqueness_of :name
-
 end
